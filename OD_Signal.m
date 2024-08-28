9bae0bcfd2b9fc54406e2ccc391a6a9a23aa1ca8
gkvaid77 <vaid.9@osu.edu>
2024-08-28 09:56:45 -0400

    Initial commit

---
+++ OD_Signal.m
@@ -0,0 +1,23 @@
+function OD_Signal(tOut,in,out,sineA,sinef,ind_start,ind_end)
+
+
+figure()
+plot(tOut,in,'k-','LineWidth',2);
+hold all;
+plot(tOut,out,'b-','LineWidth',2);
+hold off
+grid minor;
+ylabel('[units]')  
+xlabel('time [s]')
+title('Signal')
+legend('input ','output','Location','northwest')
+set(gcf,'Position',[151 246 1024 420])
+
+% calculate and plot Modulus and Phase
+sig=CalculateModPhase(tOut,in,out,sineA,sinef,ind_start,ind_end);
+
+%calculate and plot WSD
+
+[WSD_mag,WSD_phase,sig]=Calculate_WSD(sinef,sig);
+
+end
